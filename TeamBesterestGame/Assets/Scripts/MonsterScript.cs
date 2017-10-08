2eb81d810df329e7fc37eee59c4060558b1529db
ORACLE\James <jderanek@umail.iu.edu>
2017-10-08 00:03:42 -0400

    Created monster prefab with starting health and randomly generated personality traits

---
+++ TeamBesterestGame/Assets/Scripts/MonsterScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterScript : MonoBehaviour {
+
+
+    public int StartingHealth;
+    public int CurrentHealth;
+    public int Damage;
+
+    public GameObject[] Traits;
+    public GameObject Trait1;
+    public GameObject Trait2;
+
+
+    void Awake ()
+    {
+        CurrentHealth = StartingHealth;
+        int Trait1Index = Random.Range(0, Traits.Length);
+        int Trait2Index = Random.Range(0, Traits.Length);
+        Trait1 = Traits[Trait1Index];
+        Trait2 = Traits[Trait2Index];
+	}
+	
+	void Update ()
+    {
+		
+	}
+}
