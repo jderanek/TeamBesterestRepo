53e44adba597ea373cfceba9530420ad4b76b9c5
ORACLE\James <jderanek@umail.iu.edu>
2017-10-08 02:10:37 -0400

    created a button prefab that spawns and allows you to place the randomly generated monsters

--- TeamBesterestGame/Assets/Scripts/MonsterSpawner.cs
+++ TeamBesterestGame/Assets/Scripts/MonsterSpawner.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class MonsterSpawner : MonoBehaviour {
 
+    private bool MonsterGrabbed;
+    private GameObject MonsterInstance;
+    public GameObject Monster;
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        MonsterGrabbed = false;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+		if (MonsterInstance != null && MonsterGrabbed == true)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = transform.position.z - Camera.main.transform.position.z;
+            MonsterInstance.transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+        }
+
+        if (Input.GetMouseButtonDown(1) && MonsterGrabbed == true)
+        {
+            MonsterGrabbed = false;
+        }
 	}
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MonsterGrabbed = true;
+
+            MonsterInstance = Instantiate(Monster, gameObject.transform.position, Quaternion.identity);
+        }
+    }
 }
