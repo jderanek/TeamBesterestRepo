8e5472e6b6199334b6e69690720597092d6f379c
ORACLE\James <jderanek@umail.iu.edu>
2017-10-09 16:59:41 -0400

    H button now generates the resume prefab, resume prefab now has editable text

--- TeamBesterestGame/Assets/Scripts/HiringUIScript.cs
+++ TeamBesterestGame/Assets/Scripts/HiringUIScript.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class HiringUIScript : MonoBehaviour {
 
+    public GameObject Resume;
+    public Transform ResumeSpawn;
+
 	// Use this for initialization
-	void Start () {
+	void Start ()
+    {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+    {
 		
 	}
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(Resume, ResumeSpawn.position, Quaternion.identity);
+        }
+    }
 }
