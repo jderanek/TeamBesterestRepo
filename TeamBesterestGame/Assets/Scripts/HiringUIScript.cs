2550f17ef84e58d1a32024cc6491b4d9834e65e4
ORACLE\James <jderanek@umail.iu.edu>
2017-10-17 00:24:59 -0400

    Started making the monster affect the resume text, ran into a weird bug

--- TeamBesterestGame/Assets/Scripts/HiringUIScript.cs
+++ TeamBesterestGame/Assets/Scripts/HiringUIScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HiringUIScript : MonoBehaviour {
 
@@ -11,6 +12,11 @@ public class HiringUIScript : MonoBehaviour {
     public GameObject Monster;
     public GameObject MonsterInstance;
 
+    public Text Trait1Text;
+    public Text Trait2Text;
+    public Text HealthText;
+    public Text DamageText;
+
 	// Use this for initialization
 	void Start ()
     {
@@ -20,8 +26,17 @@ public class HiringUIScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-		
-	}
+        if (ResumeUp)
+        {
+            /*
+            Trait1Text = GameObject.Find("Trait1Text").GetComponent<Text>();
+            Trait2Text = GameObject.Find("Trait2Text").GetComponent<Text>();
+            HealthText = GameObject.Find("HealthText").GetComponent<Text>();
+            DamageText = GameObject.Find("DamageText").GetComponent<Text>();
+            HealthText.text = "Health " + MonsterInstance.GetComponent<MonsterScript>().StartingHealth;
+            //*/
+        }
+    }
 
     void OnMouseOver()
     {
