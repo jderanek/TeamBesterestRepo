7316e644630446f1361184885206722f10f91707
ADS\jderanek <jderanek@umail.iu.edu>
2017-10-30 18:38:45 -0400

    doors and door removal working, still can't re-close a door though

---
+++ TeamBesterestGame/Assets/Scripts/DoorScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorScript : MonoBehaviour {
+
+    public bool inConstructionMode;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        inConstructionMode = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().inConstructionMode;
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1) && inConstructionMode)
+        {
+            this.gameObject.SetActive(false);
+        }
+    }
+
+}
