e03b7a7240703588dfceb9113112bd10c1838057
ADS\nsshipma <nsshipman@gmail.com>
2017-11-08 11:03:55 -0500

    Room grid thingy work now

--- TeamBesterestGame/Assets/Scripts/PickUpRoom.cs
+++ TeamBesterestGame/Assets/Scripts/PickUpRoom.cs
@@ -6,35 +6,44 @@ public class PickUpRoom : MonoBehaviour {
 
     private bool inHand = false;
     public bool inConstructionMode;
+    private GameManager gameManager;
 
+    private int roundedX;
+    private int roundedY;
+
+    public RoomScript roomScript;
 
 	// Use this for initialization
 	void Awake ()
     {
-        
+        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
+        roomScript = gameObject.GetComponent<RoomScript>();
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        inConstructionMode = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>().inConstructionMode;
+        inConstructionMode = gameManager.inConstructionMode;
 
         if (inHand)
         {
             Vector3 mousePos = Input.mousePosition;
             mousePos.z = transform.position.z - Camera.main.transform.position.z;
             transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+            roundedX = Mathf.RoundToInt(transform.position.x);
+            roundedY = Mathf.RoundToInt(transform.position.y);
             transform.position = new Vector3(
-                Mathf.Round(transform.position.x),
-                Mathf.Round(transform.position.y),
+                roundedX,
+                roundedY,
                 0f); //Mathf.Round(transform.position.z));
             if (Input.GetMouseButtonDown(1))
             {
                 inHand = false;
-                if (this.GetComponent<RoomScript>() != null)
-                {
-                    //his.GetComponent<RoomScript>().
-                }
+                gameManager.roomList[roundedX, roundedY] = gameObject;
+                roomScript.myX = roundedX;
+                roomScript.myY = roundedY;
+                roomScript.UpdateNeighbors();
+                print(gameManager.roomList[roundedX, roundedY]);
             }
         }
     }
--- TeamBesterestGame/Assets/Scripts/RoomScript.cs
+++ TeamBesterestGame/Assets/Scripts/RoomScript.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class RoomScript : MonoBehaviour
 {
+    public GameManager gameManager;
 
     public List<GameObject> roomMembers = new List<GameObject>();
     private GameObject heldObject;
+    public int myX;
+    public int myY;
 
     public List<GameObject> heroesInRoom = new List<GameObject>();
 
@@ -18,7 +21,7 @@ public class RoomScript : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-
+        gameManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameManager>();
     }
 
     // Update is called once per frame
@@ -40,5 +43,34 @@ public class RoomScript : MonoBehaviour
                 print(roomMembers[0].GetComponent<MonsterScript>().monsterName);
             }
         }
+    }
+
+    public void UpdateNeighbors()
+    {
+        
+        if (gameManager.roomList[myX, myY + 1] != null)
+        {
+            northRoom = gameManager.roomList[myX, myY + 1];
+            northRoom.GetComponent<RoomScript>().southRoom = gameObject;
+        }
+
+        if (gameManager.roomList[myX, myY - 1] != null)
+        {
+            southRoom = gameManager.roomList[myX, myY - 1];
+            southRoom.GetComponent<RoomScript>().northRoom = gameObject;
+        }
+
+        if (gameManager.roomList[myX + 1, myY] != null)
+        {
+            eastRoom = gameManager.roomList[myX + 1, myY];
+            eastRoom.GetComponent<RoomScript>().westRoom = gameObject;
+        }
+
+        if (gameManager.roomList[myX - 1, myY] != null)
+        {
+            westRoom = gameManager.roomList[myX - 1, myY];
+            westRoom.GetComponent<RoomScript>().eastRoom = gameObject;
+        }
+        
     }
 }
