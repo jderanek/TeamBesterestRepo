faf5afb178eaaf21ed85f42a51e8c8962c2f4a01
Avery <acortell@umail.iu.edu>
2018-03-24 00:27:30 -0400

    Started work on basic Monster stuff

--- TeamBesterestGame/Assets/Scripts/Monster Scripts/BaseMonster.cs
+++ TeamBesterestGame/Assets/Scripts/Monster Scripts/BaseMonster.cs
@@ -4,13 +4,75 @@ using UnityEngine;
 
 public class BaseMonster : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
+	//Private variables for this monsters stats
+	int curHealth;
+	int maxHealth;
+	int curDamage;
+	int damage;
+	TraitBase personality;
+	int salary;
+	float stress;
+	float morale;
+	float stressGain;
+	float vacationStressLoss;
+	int infamyGain;
+	int threat;
+	int armor;
+	bool inCombat;
+	bool hasFought;
+
+	///<summary>
+	///Assigns all stats to this monster, to be used in place of super.
+	/// Defaults stress and morale, as well as gain, loss and infamy
+	///</summary>
+	/// <param name="hp">Total Health</param>
+	/// <param name="dam">Base Damage</param>
+	/// <param name="trait">Personality Trait</param>
+	/// <param name="sal">Base Salary</param>
+	/// <param name="thr">Threat</param>
+	/// <param name="arm">Base Armor</param>
+	public void AssignStats(int hp, int dam, TraitBase trait, int sal, int thr, int arm) {
+		this.maxHealth = hp;
+		this.curHealth = maxHealth;
+		this.damage = dam;
+		this.curDamage = this.damage;
+		this.personality = trait;
+		this.salary = sal;
+		this.threat = thr;
+		this.stress = 0f;
+		this.morale = .5f;
+		this.stressGain = .02f;
+		this.vacationStressLoss = .15f;
+		this.infamyGain = 1;
+		this.armor = arm;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	//Getters for most stats
+	public int getCurHealth() {
+		return this.curHealth;
+	}
+	public int getMaxHealth() {
+		return this.maxHealth;
+	}
+	public int getCurDamage() {
+		return this.curDamage;
+	}
+	public int getBaseDamage() {
+		return this.damage;
+	}
+	public TraitBase getTrait() {
+		return this.personality;
+	}
+	public int getSalary() {
+		return this.salary;
+	}
+	public int getThreat() {
+		return this.threat;
+	}
+	public float getMorale() {
+		return this.morale;
+	}
+	public float getStress() {
+		return this.stress;
 	}
 }
