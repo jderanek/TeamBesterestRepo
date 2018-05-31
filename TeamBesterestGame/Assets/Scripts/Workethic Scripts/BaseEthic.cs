b94151a537127128895c69c275bc8dedd08d94a7
Avery <acortell@umail.iu.edu>
2018-05-31 15:59:09 -0400

    Started trying to think of how to implement work ethics

---
+++ TeamBesterestGame/Assets/Scripts/Workethic Scripts/BaseEthic.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Changes some of monsters stats, such as damage
+public class BaseEthic {
+	//Private variables
+	string name;
+	string desc;
+
+}
