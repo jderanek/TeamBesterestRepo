2d7afeb23db23b9adc80cc26c178fb61570d502f
Nathaniel Shipman <nsshipman@gmail.com>
2018-09-24 21:32:37 -0400

    All Clock Buttons working and animated

---
+++ TeamBesterestGame/Assets/SkipButtonAnimator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkipButtonAnimator : MonoBehaviour {
+
+    public Sprite skipButton;
+    public Sprite skipButtonDepressed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnMouseDown()
+    {
+        GetComponent<Image>().sprite = skipButtonDepressed;
+    }
+
+    public void OnMouseUp()
+    {
+        GetComponent<Image>().sprite = skipButton;
+    }
+}
