7071c89a783418b03449a556468cdfc60d8b0bd8
Nathaniel Shipman <nsshipman@gmail.com>
2019-01-21 20:57:47 -0500

    Created some basic drafts of portrait animations

---
+++ TeamBesterestGame/Assets/Scripts/BasePortrait.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasePortrait : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Animator animator = GetComponent<Animator>();
+        animator.SetTrigger("BobFlag");
+        animator.SetTrigger("MoveRightFlag");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
