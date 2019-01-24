2b913474570be2494bfdc0aa85c41993633003e7
COMPY\James Deranek <jderanek36@gmail.com>
2019-01-24 02:34:53 -0500

    created global variable manager, added a text object to the ui bar for shifts

---
+++ TeamBesterestGame/Assets/GlobalFlagManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Yarn.Unity;
+
+public class GlobalFlagManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    [YarnCommand("CharacterDeath")]
+    public void CharacterDeath(string character)
+    {
+
+    }
+}
