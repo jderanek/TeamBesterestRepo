91b8c6459d53f4c13410717e5b2645b295ade23c
ORACLE\James <jderanek@iu.edu>
2019-03-11 17:26:38 -0400

    resolutions work, but for some reason it goes full screen and automatically sets it to 1920x1080 no matter the starting res

---
+++ TeamBesterestGame/Assets/Scripts/ResolutionManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.IO;
+
+
+public class ResolutionManager : MonoBehaviour {
+
+    public Dropdown resolutionDropdown;
+    Resolution[] resolutions;
+
+	// Use this for initialization
+	void Start ()
+    {
+
+        resolutions = Screen.resolutions;
+
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+            resolutionDropdown.options.Add(new Dropdown.OptionData(ResToString(resolutions[i])));
+
+            resolutionDropdown.value = i;
+
+            resolutionDropdown.onValueChanged.AddListener(delegate { Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, true); });
+
+        }
+    }
+
+    string ResToString(Resolution res)
+    {
+        return res.width + " x " + res.height;
+    }
+}
