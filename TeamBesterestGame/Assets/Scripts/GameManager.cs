840c041c92d64a5443b6aa21afd6e854e086405e
COMPY\James Deranek <jderanek36@gmail.com>
2019-04-04 00:15:04 -0400

    fixed followup bug

--- TeamBesterestGame/Assets/Scripts/GameManager.cs
+++ TeamBesterestGame/Assets/Scripts/GameManager.cs
@@ -242,32 +242,23 @@ public class GameManager : MonoBehaviour
         switch (shift)
         {
             case "shift1": //Goblenn and Gordon
-                if (!this.monsterList[3].GetComponent<BaseMonster>().interviewable && !this.monsterList[5].GetComponent<BaseMonster>().interviewable)
+                if (!this.monsterList[3].GetComponent<BaseMonster>().interviewable && !this.monsterList[5].GetComponent<BaseMonster>().interviewable && this.phase != "Combat")
                 {
-                    /*this.monsterList[3].GetComponent<BaseMonster>().followUpButton.gameObject.SetActive(true);
-                    this.monsterList[3].GetComponent<BaseMonster>().button.gameObject.SetActive(false);
-                    this.monsterList[5].GetComponent<BaseMonster>().followUpButton.gameObject.SetActive(true);
-                    this.monsterList[5].GetComponent<BaseMonster>().button.gameObject.SetActive(false);*/
+
                 }
                 break;
             case "shift2": //Jeff and Geoff
-                if (!this.monsterList[0].GetComponent<BaseMonster>().interviewable && !this.monsterList[1].GetComponent<BaseMonster>().interviewable)
+                if (!this.monsterList[0].GetComponent<BaseMonster>().interviewable && !this.monsterList[1].GetComponent<BaseMonster>().interviewable && this.phase != "Combat")
                 {
                     this.monsterList[0].GetComponent<BaseMonster>().dialogueRunner.GetComponent<DialogueRunner>().StartDialogue("JeffGeoffSolution1");
-                    /*this.monsterList[0].GetComponent<BaseMonster>().followUpButton.gameObject.SetActive(true);
-                    this.monsterList[0].GetComponent<BaseMonster>().button.gameObject.SetActive(false);
-                    this.monsterList[1].GetComponent<BaseMonster>().followUpButton.gameObject.SetActive(true);
-                    this.monsterList[1].GetComponent<BaseMonster>().button.gameObject.SetActive(false);*/
+
                 }
                 break;
             case "shift3": //Nilbog and Gabbin
-                if (!this.monsterList[2].GetComponent<BaseMonster>().interviewable && !this.monsterList[4].GetComponent<BaseMonster>().interviewable)
+                if (!this.monsterList[2].GetComponent<BaseMonster>().interviewable && !this.monsterList[4].GetComponent<BaseMonster>().interviewable && this.phase != "Combat")
                 {
                     this.monsterList[2].GetComponent<BaseMonster>().dialogueRunner.GetComponent<DialogueRunner>().StartDialogue("NilbogGabbinSolution1");
-                    /*this.monsterList[2].GetComponent<BaseMonster>().followUpButton.gameObject.SetActive(true);
-                    this.monsterList[2].GetComponent<BaseMonster>().button.gameObject.SetActive(false);
-                    this.monsterList[4].GetComponent<BaseMonster>().followUpButton.gameObject.SetActive(true);
-                    this.monsterList[4].GetComponent<BaseMonster>().button.gameObject.SetActive(false);*/
+
                 }
                 break;
         }
