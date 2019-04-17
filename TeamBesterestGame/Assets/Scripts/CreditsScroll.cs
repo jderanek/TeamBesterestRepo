1a825639d50125d213e5b14bf98ea99cadccf557
Avery Cortell <acortell@iu.edu>
2019-04-16 21:43:44 -0400

    Credits framework in

---
+++ TeamBesterestGame/Assets/Scripts/CreditsScroll.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreditsScroll : MonoBehaviour {
+
+    float speed = 1f;
+    RectTransform rectTrans;
+    float endX = 0f;
+    float endY = -1000f;
+
+    void Start()
+    {
+        rectTrans = gameObject.GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Vector2 newPos = Vector2.MoveTowards(rectTrans.anchoredPosition, new Vector2(endX, endY), speed);
+        rectTrans.anchoredPosition = newPos;
+    }
+}
