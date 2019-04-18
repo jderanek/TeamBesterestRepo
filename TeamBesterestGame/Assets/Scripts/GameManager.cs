10fb7923d93d470c95fe69a01bcf4da69c1b5534
COMPY\James Deranek <jderanek36@gmail.com>
2019-04-18 14:47:59 -0400

    game now loads victory scene in the Win function

--- TeamBesterestGame/Assets/Scripts/GameManager.cs
+++ TeamBesterestGame/Assets/Scripts/GameManager.cs
@@ -204,7 +204,7 @@ public class GameManager : MonoBehaviour
             this.combatManager.GetComponent<InterviewVariableStorage>().GetValue("$shift3Success").AsBool == true)
          {
             //play victory yarn file
-
+            SceneManager.LoadScene("Victory");
          }
     }
 
