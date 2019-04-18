7997e9f9ad1a09341fcac5b636892c74b3c604b1
RavenStadtmiller <ravenstad@gmail.com>
2019-04-18 12:40:29 -0400

    Revert "Bunch of fixes that nate did that i cant remember off the top of my head"
    This reverts commit 950e26e5bb52777f84f7a8c2dcccd41bb962f699.

--- TeamBesterestGame/Assets/Scripts/GameManager.cs
+++ TeamBesterestGame/Assets/Scripts/GameManager.cs
@@ -116,18 +116,10 @@ public class GameManager : MonoBehaviour
         this.gameObject.GetComponentInChildren<InterviewManager>().ExitInterview();
     }
 
-    IEnumerator ShortDelay()
-    {
-        phaseButton.interactable = false;
-        yield return new WaitForSeconds(0.5f);
-        phaseButton.interactable= true;
-    }
-
     //Starts the next phase when the button is clicked.
     //Only works when canSkip is true
     public void StartPhase()
     {
-        StartCoroutine(ShortDelay());
         SoundManager.StopDialogue();
         if (!canSkip)
             return;
@@ -207,23 +199,13 @@ public class GameManager : MonoBehaviour
     //set the combat manager's yarn variables according to each shifts points
     public void Win()
     {
-         
          if (this.combatManager.GetComponent<InterviewVariableStorage>().GetValue("$shift1Success").AsBool == true && 
             this.combatManager.GetComponent<InterviewVariableStorage>().GetValue("$shift2Success").AsBool == true && 
             this.combatManager.GetComponent<InterviewVariableStorage>().GetValue("$shift3Success").AsBool == true)
          {
             //play victory yarn file
-            StartCoroutine(LoadWin());
-        }
-    }
 
-    
-
-    IEnumerator LoadWin()
-    {
-        transition.SetActive(true);
-        yield return new WaitForSeconds(3);
-        transition.SetActive(false);
+         }
     }
 
     //Resets the dungeon to the original state, but keeps monster changes
@@ -348,15 +330,9 @@ public class GameManager : MonoBehaviour
         {
             ToggleInterviewMenu();
         }
-        if (Input.GetKeyDown("w"))
-        {
-            Win();
-        }
-
-        /*
         if (Input.GetKeyDown(KeyCode.J))
         {
-            ToggleNotebook();
+            this.ToggleNotebook();
             uiManager.TogglePrompts("1");
         }
         /*if (Input.GetKeyDown(KeyCode.K))
